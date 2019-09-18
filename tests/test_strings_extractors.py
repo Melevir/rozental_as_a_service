bd49c91c3616bb33e1edb7f7f8eface8670e94b4
Artem Kuzmenko <arkuzka@mail.ru>
2019-09-18 18:20:05 +0300

    changed strip params

--- tests/test_strings_extractors.py
+++ tests/test_strings_extractors.py
@@ -46,7 +46,7 @@ def test_extract_from_html_src():
 def test_extract_from_markdown_src():
     src = _load_src_file('src_markdown')
     actual_res = sorted(extr.extract_from_markdown(src))
-    actual_res[-1] = actual_res[-1].strip('\n').strip('\t')
+    actual_res[-1] = actual_res[-1].strip()
     print('')
     print(actual_res)
     exp_res = [
