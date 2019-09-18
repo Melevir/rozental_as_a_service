4fe145167667462eb6fa161c4ffdacc498769e60
Artem Kuzmenko <arkuzka@mail.ru>
2019-09-18 10:59:56 +0300

    added debug prints

--- tests/test_strings_extractors.py
+++ tests/test_strings_extractors.py
@@ -46,7 +46,9 @@ def test_extract_from_html_src():
 def test_extract_from_markdown_src():
     src = _load_src_file('src_markdown')
     actual_res = sorted(extr.extract_from_markdown(src))
-    assert actual_res == sorted([
+    print('')
+    print(actual_res)
+    exp_res = [
         '\n',
         '\n',
         '\n',
@@ -57,7 +59,9 @@ def test_extract_from_markdown_src():
         ' Здесь <span>1</span> <br/>2 ',
         'Title',
         'strong 55 77',
-    ])
+    ]
+    print(exp_res)
+    assert actual_res == exp_res
 
 
 def test_extract_from_js():
