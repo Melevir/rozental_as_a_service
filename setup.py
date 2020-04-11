7e8cf6541e5fc5d818309d3813040d564b64c62a
melevir <melevir@gmail.com>
2020-04-11 12:36:14 +0300

    bye, python 3.6

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Quality Assurance',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
